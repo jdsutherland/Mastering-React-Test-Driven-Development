059fbe5d5e6cbfbcf94f7f0c12e2b53be61a32b9
Jeff Sutherland <jeffdsuth@gmail.com>
2020-07-02 19:07:54 -0700

    Add ex - appt table

--- appointments/src/AppointmentsDayView.js
+++ appointments/src/AppointmentsDayView.js
@@ -5,8 +5,45 @@ const appointmentTimeOfDay = startsAt => {
   return `${h}:${m}`;
 }
 
-export const Appointment = ({ customer: { firstName } }) => (
-  <div>{firstName}</div>
+export const Appointment = ({
+  customer,
+  stylist,
+  service,
+  notes,
+  startsAt,
+}) => (
+  <div id="appointmentView">
+    <h3>
+      Today&rsquo;s appointment at {appointmentTimeOfDay(startsAt)}
+    </h3>
+    <table>
+      <tbody>
+        <tr>
+          <td>Customer</td>
+          <td>
+            {customer.firstName} {customer.lastName}
+          </td>
+        </tr>
+        <tr>
+          <td>Phone number</td>
+          <td>{customer.phoneNumber}</td>
+        </tr>
+        <tr>
+          <td>Stylist</td>
+          <td>{stylist}</td>
+        </tr>
+        <tr>
+          <td>Service</td>
+          <td>{service}</td>
+        </tr>
+        <tr>
+          <td>Notes</td>
+          <td>{notes}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
 );
 
 export const AppointmentsDayView = ({ appointments }) => {
--- appointments/test/AppointmentsDayView.test.js
+++ appointments/test/AppointmentsDayView.test.js
@@ -5,7 +5,7 @@ import { Appointment, AppointmentsDayView } from '../src/AppointmentsDayView';
 
 describe('Appointment', () => {
   let container
-  let customer
+  let customer = {}
 
   beforeEach(() => {
     container = document.createElement('div');
@@ -13,21 +13,59 @@ describe('Appointment', () => {
 
   const render = component => ReactDOM.render(component, container)
 
+  const appointmentTable = () =>
+    container.querySelector('#appointmentView > table');
+
+  it('renders a table', () => {
+    render(<Appointment customer={customer} />);
+    expect(appointmentTable()).not.toBeNull();
+  });
+
   it('renders the customer first name', () => {
     customer = { firstName: 'Ashley' };
-
     render(<Appointment customer={customer} />);
-
-    expect(container.textContent).toMatch('Ashley');
+    expect(appointmentTable().textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
     customer = { firstName: 'Jordan' };
-
     render(<Appointment customer={customer} />);
-
-    expect(container.textContent).toMatch('Jordan');
+    expect(appointmentTable().textContent).toMatch('Jordan');
   });
+
+  it('renders the customer phone number', () => {
+    customer = { phoneNumber: '1234567' };
+    render(<Appointment customer={customer} />);
+    expect(appointmentTable().textContent).toMatch('1234567');
+  });
+
+  it('renders the stylist name', () => {
+    render(<Appointment customer={customer} stylist='Jo' />);
+    expect(appointmentTable().textContent).toMatch('Jo');
+  });
+
+  it('renders the salon service', () => {
+    render(<Appointment customer={customer} service='Blow' />);
+    expect(appointmentTable().textContent).toMatch('Blow');
+  });
+
+  it('renders the notes', () => {
+    render(<Appointment customer={customer} notes='abc' />);
+    expect(appointmentTable().textContent).toMatch('abc');
+  });
+
+  it('renders the heading with the time', () => {
+    const today = new Date();
+    const timestamp = today.setHours(9, 0, 0)
+    render(
+      <Appointment customer={customer} startsAt={timestamp} />
+    );
+    expect(container.querySelector('h3')).not.toBeNull()
+    expect(container.querySelector('h3').textContent).toEqual(
+      'Today’s appointment at 09:00'
+    )
+  });
+
 });
 
 describe('AppointmentsDayView', () => {
