11b1301b9b3ee65eefac5aacddadfdb458da5762
Jeff Sutherland <jeffdsuth@gmail.com>
2020-07-06 20:00:50 -0700

    Add appointment page to app for manual testing

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -150,6 +150,7 @@ export const AppointmentForm = ({
       checkedTimeSlot={appointment.startsAt}
       handleChange={handleStartsAtChange}
     />
+    <input type="submit" value="Add" />
   </form>
 }
 
