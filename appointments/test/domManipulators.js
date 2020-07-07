e555376458f6ae26e621d96117d3ce53c0b84c17
Jeff Sutherland <jeffdsuth@gmail.com>
2020-07-07 13:23:43 -0700

    Formatting

--- appointments/test/domManipulators.js
+++ appointments/test/domManipulators.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 export const createContainer = () => {
   const container = document.createElement('div')
 
+  const element = selector => container.querySelector(selector);
   const form = id => container.querySelector(`form[id=${id}]`)
   const field = (formId, name) => form(formId).elements[name]
   const labelFor = formElement =>
     container.querySelector(`label[for="${formElement}"]`)
-  const element = selector => container.querySelector(selector);
 
   return {
     element,
