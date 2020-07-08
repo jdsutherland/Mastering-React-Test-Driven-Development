2e955c98ce9685ec63f9ffaf854b4c61210a0529
Jeff Sutherland <jeffdsuth@gmail.com>
2020-07-08 15:03:05 -0700

    it passes props through to children

--- appointments/test/AppointmentFormLoader.test.js
+++ appointments/test/AppointmentFormLoader.test.js
@@ -51,4 +51,13 @@ describe('AppointmentFormLoader', () => {
     expect(AppointmentFormExports.AppointmentForm)
       .toHaveBeenLastCalledWith({availableTimeSlots}, expect.anything());
   });
+
+  it('passes props through to children', async() => {
+    await renderAndWait(<AppointmentFormLoader testProp={123}/>);
+
+    expect(AppointmentFormExports.AppointmentForm).toHaveBeenCalledWith(
+        expect.objectContaining({ testProp: 123 }),
+        expect.anything()
+      );
+  });
 });
