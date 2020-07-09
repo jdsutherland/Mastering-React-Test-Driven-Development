2edbcb3b5a8ff5f2170219c99deb0c93e011a420
Jeff Sutherland <jeffdsuth@gmail.com>
2020-07-08 17:35:06 -0700

    it initially shows the AppointmentDayViewLoader

---
+++ appointments/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppointmentsDayViewLoader } from '../src/AppointmentsDayViewLoader';
+
+export const App = () => (
+  <React.Fragment>
+    <AppointmentsDayViewLoader />
+  </React.Fragment>
+)
---
+++ appointments/test/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  createShallowRenderer,
+  type
+} from './shallowHelpers';
+import { App } from '../src/App';
+import { AppointmentsDayViewLoader } from '../src/AppointmentsDayViewLoader';
+
+describe('App', () => {
+  let render, elementMatching;
+
+  beforeEach(() => {
+    ({ render, elementMatching } = createShallowRenderer());
+  });
+
+  it('initially shows the AppointmentDayViewLoader', () => {
+    render(<App />);
+    expect(
+      elementMatching(type(AppointmentsDayViewLoader))
+    ).toBeDefined();
+  });
+});
